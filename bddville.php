5793f07d4faf241eef49ca6804736a028dd1a7ec
Aline86 <ca.haestie@gmail.com>
2020-02-11 11:05:22 +0100

    scrollbar

--- bddville.php
+++ bddville.php
@@ -14,7 +14,7 @@ $cp=isset($_GET['cp'])?$_GET['cp'].'%':"";
 
 if($ville!=""){
 
-    $sth2 = 'SELECT ville_nom, ville_code_postal as postal FROM villes WHERE ville_nom LIKE "'.$ville.'" ORDER BY ville_nom ASC LIMIT 10 ';
+    $sth2 = 'SELECT ville_nom, ville_code_postal as postal FROM villes WHERE ville_nom LIKE "'.$ville.'" ORDER BY ville_nom ASC LIMIT 30';
     $reponse = $bdd->query($sth2);
     
     $donnees=$reponse->fetchAll(PDO::FETCH_ASSOC);
@@ -22,7 +22,7 @@ if($ville!=""){
     }
 if($cp!=""){
 
-    $sth2 = 'SELECT ville_nom, ville_code_postal as postal FROM villes WHERE ville_code_postal LIKE "'.$cp.'" ORDER BY ville_code_postal ASC LIMIT 10 ';
+    $sth2 = 'SELECT ville_nom, ville_code_postal as postal FROM villes WHERE ville_code_postal LIKE "'.$cp.'" ORDER BY ville_code_postal ASC LIMIT 30';
     $reponse = $bdd->query($sth2);
     
     $donnees=$reponse->fetchAll(PDO::FETCH_ASSOC);
--- villes.php
+++ villes.php
@@ -2,33 +2,48 @@
 <link rel="stylesheet" href="style.css">
 <form method="get" action="">  
 <input id="ville" type="text" name="ville"  value="<?php echo $ville=isset($_GET['ide'])? $_GET['ide']: $ville=""; ?>" placeholder="<?php echo $ville; ?>" />
-<input id="cp" type="text" name="cp"  value="<?php echo $ville=isset($_GET['cp'])? $_GET['cp']: $ville=""; ?>" placeholder="<?php echo $ville; ?>" />
+<input id="cp" type="text" name="cp" class="cp"  value="<?php echo $ville=isset($_GET['cp'])? $_GET['cp']: $ville=""; ?>" placeholder="<?php echo $ville; ?>" />
 <input type="submit" />
-<div id="villesDeFrance" class="villes"></div>
-<div id="postal" class="postal"></div>
+<div class="flex">
+  <div id="villesDeFrance" class="villes"></div>
+  <div id="postal" class="postal"></div>
+</div>
 </form>
 
 
 <script>
 
-function myFunction(arr, linkinfo) {
+function myFunction(arr, value, villecp, div, linkinfo) {
         console.log(arr)
-        document.getElementById('villesDeFrance').innerHTML=""
+        
+    document.getElementById(div).classList.add('disable')
+    
+        document.getElementById(div).innerHTML=""
         tableau_ville=[]
         tableau_code=[]
         for (i=0; i<arr.length; i++){
+         
+          if(arr[i].postal.includes('-')){
+            villestirets=arr[i].postal.split("-")
+           console.log(villestirets)
+           for(i=0; i<villestirets.length; i++){
+              tableau_code.push(villestirets[i])
+            }
+                 
+           }
+          
+        }
+           for (i=0; i<arr.length; i++){
           
             tableau_ville.push(arr[i].ville_nom)
-            tableau_code.push(arr[i].postal)        
-           }
-          console.log(tableau_ville)
-       
-      var autocomplete=document.getElementById('villesDeFrance')
+            tableau_code.push(arr[i].postal) 
+          }
+      var autocomplete=document.getElementById(div)
       element=[];   
       lien=[]
       for(i=0; i<tableau_ville.length; i++){
         essai1=document.createElement('a')
-        essai1.setAttribute("href", "villes2.php?ide="+tableau_ville[i]+"&cp="+tableau_code[i])
+        essai1.setAttribute("href", "villes.php?ide="+tableau_ville[i]+"&cp="+tableau_code[i])
    
         lien.push(essai1)
       }
@@ -43,15 +58,42 @@ function myFunction(arr, linkinfo) {
       autocomplete.appendChild(lien[i]).append(' '+tableau_code[i])
       autocomplete.append(sauts[i])
     }
+
+    if(value=="32" || document.getElementById(villecp).value==="" ){
+    
+        erase_childs(document.getElementById(div))
+       
+    }
+  
+    if(document.getElementById(villecp).value==="" ){
+      document.getElementById(div).classList.add('disable2')
+    }else{
+      document.getElementById(div).classList.remove('disable2')
+    }
+
+
     if(mot!=""){
-        mot=document.getElementById('ville').value.substring(0, mot.length); 
+        mot=document.getElementById(villecp).value.substring(0, mot.length); 
     }  
     if(code!=""){
-        code=document.getElementById('ville').value.substring(0, code.length); 
+        code=document.getElementById(villecp).value.substring(0, code.length); 
     }   
-    console.log(document.getElementById('villesDeFrance'))        
+    console.log(document.getElementById(div))        
     }   
     
+    function erase_childs(node){
+	    if(node.childNodes){
+        
+		  var childs=node.childNodes;
+		  for(var i=0;i<childs.length;i++){
+			node.removeChild(childs[i]);
+      console.log('ok')
+		  }
+	  }
+    
+}
+
+
 var input=document.getElementById('ville');
 
 mot=""
@@ -59,34 +101,37 @@ mot=""
 input.addEventListener('keyup', function(e){
 
    if(e.keyCode!=8){
-    
+      var evenement=e.keyCode
+     
      if(mot+=new String(String.fromCharCode(e.keyCode))){
         console.log(mot)
         villeFrance=document.getElementById("ville").value
         mot=villeFrance.substring(0, villeFrance.length)
         var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "ville.php?id="+mot, true);
+        xhttp.open("GET", "bddville.php?id="+mot, true);
         xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var myArr = JSON.parse(this.responseText);
-          myFunction(myArr)  
+          myFunction(myArr, evenement, "ville", "villesDeFrance")  
           }
       }
     xhttp.send();
            
     }
     } else if(e.keyCode==8) {
-
+ 
+      var evenement=e.keyCode
     villeFrance=document.getElementById("ville").value
-    villeFrance=villeFrance.substring(0, villeFrance.length)
+    
+    mot=villeFrance.substring(0, villeFrance.length)
      
     console.log(villeFrance);
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "ville.php?id="+villeFrance, true);
+    xhttp.open("GET", "bddville.php?id="+villeFrance, true);
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
-        myFunction(myArr) 
+        myFunction(myArr, evenement, "ville", "villesDeFrance") 
 	}
 
       }
@@ -101,6 +146,8 @@ code=[]
 cp.addEventListener('keyup', function(e){
    
    if(e.keyCode!=8){
+
+    var evenement=e.keyCode
     if (e.keyCode>=96 && e.keyCode<=105)
         {
         code+=e.keyCode-96;
@@ -110,34 +157,36 @@ cp.addEventListener('keyup', function(e){
         cpFrance=document.getElementById("cp").value
         code=cpFrance.substring(0, cpFrance.length)
         var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "ville.php?cp="+code, true);
+        xhttp.open("GET", "bddville.php?cp="+code, true);
         xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var myArr = JSON.parse(this.responseText);
-          myFunction(myArr)  
+          myFunction(myArr, evenement, "cp", "postal")  
           }
       }
     xhttp.send();
            
     
     } else if(e.keyCode==8) {
-      
+  
     cpFrance=document.getElementById("cp").value
+  
     code=cpFrance.substring(0, cpFrance.length)
 
    
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "ville.php?cp="+code, true);
+    xhttp.open("GET", "bddville.php?cp="+code, true);
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
-        myFunction(myArr) 
+        myFunction(myArr, evenement, "cp", "postal") 
 	}
 
       }
     xhttp.send(); 
     }
 })
+
 
 </script>
 
